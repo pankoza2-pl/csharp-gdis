d1fc00b7f94abcfb115f14a9d3e7d48908a471c5
kapi2.0peys <107350496+kapi20peys@users.noreply.github.com>
2022-07-12 21:11:29 +0800

    Update EllipticInvert.cs

--- EllipticInvert.cs
+++ EllipticInvert.cs
@@ -9,8 +9,12 @@ static extern IntPtr CreateEllipticRgn(int nLeftRect, int nTopRect,int nRightRec
 [DllImport("gdi32.dll")]
 private static extern bool PatBlt(IntPtr hdc, int nXLeft, int nYLeft, int nWidth, int nHeight, CopyPixelOperation dwRop);
 
+[DllImport("user32.dll", SetLastError = true)]
+private static extern IntPtr GetDC(IntPtr hWnd);
+
 private static void Main()
 {
+    IntPtr hdc = GetDC(IntPtr.Zero);
     Random random = new Random
     IntPtr hd = CreateEllipticRgn(random.Next(screenW), random.Next(screenH), random.Next(screenW), random.Next(screenH));
     SelectObject(hdc, hd);
